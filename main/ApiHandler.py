06dc41eb5f11c30470c22b22d4abbb9bbf188baf
Silewis37 <silewis37@gmail.com>
2025-04-19 01:14:12 -0400

    Updated Master Files and Ran Some Tests on new features. ALL WIP AS OF THIS MOMENT!!!! Update.

--- main/ApiHandler.py
+++ main/ApiHandler.py
@@ -96,8 +96,8 @@ class Github:
         f.write(data4)
         f.close()
     
-    def RepoForks(self, Owner, Repo, filePath):
-      url = f"https://api.github.com/repos/{Owner}/{Repo}/forks"
+    def RepoTagsList(self, Owner, Repo, filePath):
+      url = f"https://api.github.com/repos/{Owner}/{Repo}/tags"
       h = {
       "Accept": "application/vnd.github+json",
       "Authorization": f"Bearer {self.token}",
@@ -109,9 +109,66 @@ class Github:
         data4 = json.dumps(response.json(), indent=4)
         f.write(data4)
         f.close()
+    
+    def RepoTagsList(self, Owner, Repo, filePath):
+      url = f"https://api.github.com/repos/{Owner}/{Repo}/tags"
+      h = {
+      "Accept": "application/vnd.github+json",
+      "Authorization": f"Bearer {self.token}",
+      "X-GitHub-Api-Version": "2022-11-28"
+      }
+      response = requests.get(url, headers=h)
+      print(response.json())
+      with open(filePath, "w+") as f:
+        data4 = json.dumps(response.json(), indent=4)
+        f.write(data4)
+        f.close()
+
   class User:
     def UserStarredList(self, filePath):
       url = f"https://api.github.com/user/starred"
+      h = {
+      "Accept": "application/vnd.github+json",
+      "Authorization": f"Bearer {self.token}",
+      "X-GitHub-Api-Version": "2022-11-28"
+      }
+      response = requests.get(url, headers=h)
+      print(response.json())
+      with open(filePath, "w+") as f:
+        data4 = json.dumps(response.json(), indent=4)
+        f.write(data4)
+        f.close()
+    
+    def UserFollowersList(self, filePath):
+      url = f"https://api.github.com/user/followers"
+      h = {
+      "Accept": "application/vnd.github+json",
+      "Authorization": f"Bearer {self.token}",
+      "X-GitHub-Api-Version": "2022-11-28"
+      }
+      response = requests.get(url, headers=h)
+      print(response.json())
+      with open(filePath, "w+") as f:
+        data4 = json.dumps(response.json(), indent=4)
+        f.write(data4)
+        f.close()
+        
+    def UserFollowingList(self, filePath):
+      url = f"https://api.github.com/user/following"
+      h = {
+      "Accept": "application/vnd.github+json",
+      "Authorization": f"Bearer {self.token}",
+      "X-GitHub-Api-Version": "2022-11-28"
+      }
+      response = requests.get(url, headers=h)
+      print(response.json())
+      with open(filePath, "w+") as f:
+        data4 = json.dumps(response.json(), indent=4)
+        f.write(data4)
+        f.close()
+        
+    def UserRepositoryList(self, username,filePath):
+      url = f"https://api.github.com/users/{username}/repos"
       h = {
       "Accept": "application/vnd.github+json",
       "Authorization": f"Bearer {self.token}",
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jarvis_api_library",
-    version="1.1.1",
+    version="1.1.2",
     packages=find_packages(),
     install_requires=[
         "requests>=2.32.3",
